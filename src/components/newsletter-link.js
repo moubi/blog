cf05d3ae739805242931bc1aa4ddcfb1287b2b48
Miroslav Nikolov <miroslavnikolov@ymail.com>
2021-03-04 11:45:26 +0100

    Add newsletter icon on page's top.

--- src/components/newsletter-link.js
+++ src/components/newsletter-link.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { rhythm } from "../utils/typography"
+import mailIcon from "../../content/assets/mail.svg"
 
 import styles from "./newsletter-link.module.css"
 
@@ -13,6 +14,7 @@ export default function NewsletterLink({ isInBlogPost }) {
         marginBottom: isInBlogPost ? rhythm(1.7) : 0,
       }}
     >
+      <img src={mailIcon} alt="Newsletter mail icon" />
       <Link to="/subscribe" title="webup.org/blog's newsletter">
         Get the monthly newsletter
       </Link>
